999e756a11754f48faa1734afb6ba8a3ce766cd9
guilherme-dev-ei <guilherme@eventodaigreja.com.br>
2026-02-20 22:29:37 -0300

    #3

---
+++ src/app/pages/auth/create-account/create-account.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account',
+  imports: [],
+  templateUrl: './create-account.html',
+  styleUrls: ['./create-account.scss'],
+})
+export class CreateAccountComponent { }
