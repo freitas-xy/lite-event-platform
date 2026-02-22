b2ae7c7aed63b18b1ab7756d76875082f442ea27
guilherme-dev-ei <guilherme@eventodaigreja.com.br>
2026-02-22 19:47:13 -0300

    #24

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://joobuifwxpddwbhpujmw.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impvb2J1aWZ3eHBkZHdiaHB1am13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NzUxNzEsImV4cCI6MjA4NzI1MTE3MX0.sfkz3WIT95qrPvVO2Jk-oPgZd0cPwdhwtlHMXrD6hu4'
+  supabaseUrl: '-- SUPABASE_URL --',
+  supabaseAnonKey: '-- SUPABASE_ANON_KEY --',
 }
